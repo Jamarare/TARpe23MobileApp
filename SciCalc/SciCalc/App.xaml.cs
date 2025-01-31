1c029433d957f72197d39e4944b998e0e1f8fbaf
opilane <opilane@HP-CZC0359NN8>
2025-01-31 14:08:04 +0200

    .

--- SciCalc/SciCalc/App.xaml.cs
+++ SciCalc/SciCalc/App.xaml.cs
@@ -1,6 +1,7 @@
 ﻿#if WINDOWS
 using Microsoft.UI;
 using Microsoft.UI.Windowing;
+using Views;
 using Windows.Graphics;
 #endif
 
@@ -28,7 +29,7 @@ namespace SciCalc
             });
         #endif
 
-            MainPage = new AppShell();
+            MainPage = new Views.CalculatorPage();
         }
     }
 }
