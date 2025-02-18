6aa0eb187e94fc114abf6aee775496bb3d3646ab
opilane <opilane@HP-CZC0359NN9>
2025-02-18 12:53:21 +0200

    .

--- MauiPlanets/MauiPlanets/Views/PlanetsDetailsPage.xaml.cs
+++ MauiPlanets/MauiPlanets/Views/PlanetsDetailsPage.xaml.cs
@@ -4,10 +4,14 @@ namespace Views;
 
 public partial class PlanetsDetailsPage : ContentPage
 {
-	public PlanetsDetailsPage(Planet planet)
-	{
-		InitializeComponent();
+    public PlanetsDetailsPage(Planet planet)
+    {
+        InitializeComponent();
 
-		this.BindingContext = planet;
-	}
+        this.BindingContext = planet;
+    }
+    async void BackButton_Clicked(System.Object sender, System.EventArgs e)
+    {
+        await Navigation.PopAsync();
+    }
 }
